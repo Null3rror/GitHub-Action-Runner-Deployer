39a328f951b9c01b92334a6f4da7c13061c75bf6
Sepehr Razmyar <69770592+Null3rror@users.noreply.github.com>
2023-08-05 20:28:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-OWNER=$OWNER
-REPO=$REPO
-ACCESS_TOKEN=$ACCESS_TOKEN
-
-# Visit https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28 for more API calls
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
-
 cd /home/runner/actions-runner
 
-./config.sh --url https://github.com/${OWNER}/${REPO} --token ${REG_TOKEN}
+if ! [ -z $ORGANIZATION ]; then
+    echo "Creating a runner for organization: ${ORGANIZATION}..."
+    REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+    ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+elif ! [ -z $OWNER ] && ! [ -z $REPO ]; then
+    echo "Creating a runner for ${OWNER}/${REPO}..."
+    REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+    ./config.sh --url https://github.com/${OWNER}/${REPO} --token ${REG_TOKEN}
+else
+    echo "ORGANIZATION or OWNER and REPO enviromental variables were not defined!"
+    echo "Define either one of them in your docker-compose.yml"
+    echo "Exiting..."
+    exit 1
+fi
 
 cleanup() {
     echo "Removing runner..."
